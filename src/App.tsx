6e34347d7fcdc1d9717b539ce2969bb1aa6db342
karim fanti <karimwebdevelopment@gmail.com>
2021-05-03 00:43:28 +0200

    fixed some issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,207 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { Invoice } from './components/_Invoice'
 
-function App() {
+const Main = styled.main`
+  max-width: 73rem;
+  margin: 10.4rem auto 3rem;
+  padding: 0 24px;
+
+  @media screen and (min-width: 760px) {
+    margin-top: 13.6rem;
+  }
+
+  @media screen and (min-width: 1000px) {
+    margin-top: 7.2rem;
+    padding: 0;
+  }
+`
+
+const InvoicesInteractions = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const Title = styled.h1`
+  font-size: clamp(2.2rem, 5vw, 3.6rem);
+  line-height: 1.3;
+  font-weight: 600;
+  color: var(--color-white);
+  margin-bottom: 0.5rem;
+`
+
+const Counter = styled.span`
+  font-weight: 500;
+  font-size: 1.3rem;
+  line-height: 1.5rem;
+  color: var(--color-grayish-purple);
+`
+
+const LeftArea = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const Select = styled.select`
+  border: none;
+  background: transparent;
+`
+
+const AddInvoiceBtn = styled.button`
+  border-radius: 2.4em;
+  font-size: 1.2rem;
+  font-weight: 600;
+  line-height: 1.5rem;
+  color: var(--color-white);
+  background: var(--color-purple);
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  min-width: 9rem;
+  padding: 0.5em 1.4rem 0.5em 0.6rem;
+  cursor: pointer;
+  transition: filter 0.2s ease;
+  margin-left: 1.8rem;
+
+  &::after {
+    content: 'New';
+    height: 1.2rem;
+
+    @media screen and (min-width: 760px) {
+      content: 'New Invoice'
+    }
+  }
+
+  @media screen and (min-width: 760px) {
+    padding: 0.68em 1.5rem 0.68em 0.8rem;
+    min-width: 15rem;
+  }
+
+  div {
+    width: 3.2rem;
+    height: 3.2rem;
+    background: var(--color-white);
+    border-radius: 50%;
+    display: grid;
+    place-items: center;
+  }
+
+  &:hover {
+    filter: brightness(1.16);
+  }
+`
+
+const InvoicesList = styled.section`
+  margin-top: 3.2rem;
+
+  @media screen and (min-width: 750px) {
+    margin-top: 5.6rem;
+  }
+
+  @media screen and (min-width: 1000px) {
+    margin-top: 6.5rem;
+  }
+
+  ul {
+    list-style: none;
+  }
+`
+
+export default function Home() {
+  const invoices = [
+    {
+      id: 'RT3080',
+      receiver: 'Jensen Huang',
+      date: '19 Aug 2021',
+      amount: 1800.91,
+      status: 'Paid',
+    },
+    {
+      id: 'XM9141',
+      receiver: 'Alex Grim',
+      date: '20 Sep 2021',
+      amount: 556.01,
+      status: 'Pending',
+    },
+    {
+      id: 'RG0314',
+      receiver: 'John Marrison',
+      date: '01 Oct 2021',
+      amount: 14002.33,
+      status: 'Paid',
+    },
+    {
+      id: 'RT2080',
+      receiver: 'Alysa Werner',
+      date: '12 Oct 2021',
+      amount: 102.04,
+      status: 'Pending',
+    },
+    {
+      id: 'AA1449',
+      receiver: 'Mellisa Clarke',
+      date: '14 Oct 2021',
+      amount: 4032.33,
+      status: 'Pending',
+    },
+    {
+      id: 'Y9141',
+      receiver: 'Thomas Wayne',
+      date: '31 Oct 2021',
+      amount: 6155.91,
+      status: 'Pending',
+    },
+    {
+      id: 'FV2353',
+      receiver: 'Anita Wainright',
+      date: '12 Nov 2021',
+      amount: 3102.04,
+      status: 'Draft',
+    }
+  ]
+  const [isEmpty, setIsEmpty] = useState(() => invoices.length == 0 ? true : false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      {/* <Head>
+        <title>Invoicer | Home</title>
+        <meta name="description" content="The best app for creating your own invoices" />
+      </Head> */}
 
-export default App;
+      <Main>
+        <InvoicesInteractions>
+          <div>
+            <Title>Invoices</Title>
+            <Counter>7 Invoices</Counter>
+          </div>
+          <LeftArea>
+            <Select></Select>
+            <AddInvoiceBtn>
+              <div>
+                <img src="/assets/icon-plus.svg" alt="add icon" />
+              </div>
+            </AddInvoiceBtn>
+          </LeftArea>
+        </InvoicesInteractions>
+        <InvoicesList>
+          {isEmpty ? (
+            <h1>its empty</h1>
+          ) : (
+            <ul>
+              {invoices.map(invoice => <Invoice
+                id={invoice.id}
+                receiver={invoice.receiver}
+                date={invoice.date}
+                amount={invoice.amount}
+                status={invoice.status}
+                key={invoice.id}
+              />)} {/* Component */}
+            </ul>
+          )}
+        </InvoicesList>
+      </Main>
+    </>
+  )
+}
