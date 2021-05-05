e776691699e24a14956ea800a5b999c705c338e2
karim fanti <karimwebdevelopment@gmail.com>
2021-05-05 14:34:36 +0200

    added formatDate function

---
+++ src/utils/formatters.ts
@@ -0,0 +1,12 @@
+import dayjs from 'dayjs'
+
+export function formatMoneyAmount(amount: number) {
+  return (amount).toLocaleString('en')
+}
+
+export function formatDate(date: string) {
+  const dateFormat = dayjs(date, 'YYYY-MM-DD')
+  const formatedDate = dateFormat.format('DD MMM YYYY')
+
+  return formatedDate
+}
