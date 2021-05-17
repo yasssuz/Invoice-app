7f1f885e2ec7780e0eaf24e1ebd0e8344ee8fb10
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-18 01:45:12 +0200

    go back component created

---
+++ src/components/shared/_GoBack.tsx
@@ -0,0 +1,48 @@
+import { memo } from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+interface GoBackProps {
+  onClick?: () => void
+}
+
+const ContentContainer = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+
+  &:hover {
+    img {
+      transform: rotate(-180deg);
+    }
+
+    span {
+      filter: brightness(0.7);
+    }
+  }
+
+  img {
+    transform: rotate(180deg);
+    transition: transform 0.5s ease;
+  }
+
+  span {
+    margin-left: 2.4rem;
+    font-weight: bold;
+    font-size: 1.4rem;
+    color: var(--color-white);
+    transition: filter 0.3s ease;
+  }
+`
+
+export const GoBack = (props: GoBackProps) => {
+  const { onClick } = props
+  return (
+    <Link to="/" style={{ textDecoration: 'none' }}>
+      <ContentContainer onClick={onClick}>
+        <img src="/assets/icon-arrow-right.svg" alt="go back" />
+        <span>Go back</span>
+      </ContentContainer>
+    </Link>
+  )
+}
