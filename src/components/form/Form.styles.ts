ab8c99fd7c3682925a914a0b2953be130c6fba5a
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-18 23:21:59 +0200

    good amount of the form

---
+++ src/components/form/Form.styles.ts
@@ -0,0 +1,87 @@
+import styled from 'styled-components'
+
+export const FormContainer = styled.div`
+  max-width: 72rem;
+  width: 100%;
+  background: var(--color-nearly-black);
+  position: absolute;
+  z-index: 900;
+  top: 0;
+  bottom: 0;
+  padding: 10.4rem 2.4rem 0;
+  height: 300vh;
+
+  button {
+    margin-top: 10rem;
+  }
+`
+
+export const Title = styled.h1`
+  margin: 2.4rem 0;
+  font-weight: bold;
+  font-size: 2.4rem;
+  line-height: 3.2rem;
+  color: var(--color-white);
+`
+
+export const Fieldset = styled.fieldset`
+  border: none; 
+
+  & + & {
+    margin-top: 4.8rem;
+  }
+`
+
+export const Legend = styled.legend`
+  color: var(--color-purple);
+  font-size: 1.3rem;
+  line-height: 1.5rem;
+  font-weight: bold;
+`
+
+export const SmallInputsArea = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 0 2.3rem; 
+
+  .last-child {
+    grid-column: 1 /span 2;
+  }
+`
+
+export const InputBlock = styled.div`
+  margin-top: 2.4rem;
+  position: relative;
+
+  &.error {
+    label {
+      color: var(--color-red);
+    }
+
+    input {
+      border: 1px solid var(--color-red);
+    }
+  }
+`
+
+export const Label = styled.label`
+  display: block;
+  line-height: 1.5rem;
+  font-size: 1.2rem;
+  font-weight: 500;
+  color: var(--color-semi-gray);
+  margin-bottom: 1rem;
+`
+
+export const TextInput = styled.input`
+  width: 100%;
+  border-radius: 0.4rem;
+  height: 4.8rem;
+  border: 1px solid var(--color-very-light-black);
+  background: var(--color-light-black);
+  font-size: 1.2rem;
+  line-height: 1.5rem;
+  font-weight: bold;
+  color: var(--color-white);
+  padding: 0 2rem;
+`
--- src/components/form/_Form.tsx
+++ src/components/form/_Form.tsx
@@ -1,8 +1,17 @@
-import styled from 'styled-components'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from 'yup'
 import { GoBack } from '../shared/_GoBack'
+import {
+  FormContainer,
+  Title,
+  Fieldset,
+  Legend,
+  SmallInputsArea,
+  InputBlock,
+  Label,
+  TextInput
+} from './Form.styles'
 
 interface FormProps {
   handleModal: () => void
@@ -13,6 +22,12 @@ interface FormData {
   senderCity: string
   senderPostCode: string
   senderCountry: string
+  clientName: string
+  clientEmail: string
+  clientStreetAddress: string
+  clientCity: string
+  clientPostCode: string
+  clientCountry: string
 }
 
 const FormSchema = yup.object().shape({
@@ -20,12 +35,19 @@ const FormSchema = yup.object().shape({
   senderCity: yup.string().matches(/^[aA-zZ\s]+$/).required(),
   senderPostCode: yup.string().required(),
   senderCountry: yup.string().matches(/^[aA-zZ\s]+$/).required(),
+  clientName: yup.string().matches(/^[aA-zZ\s]+$/).required(),
+  clientEmail: yup.string().email().required(),
+  clientStreetAddress: yup.string().required(),
+  clientCity: yup.string().matches(/^[aA-zZ\s]+$/).required(),
+  clientPostCode: yup.string().required(),
+  clientCountry: yup.string().matches(/^[aA-zZ\s]+$/).required(),
 })
 
 export function Form(props: FormProps) {
   const { handleModal } = props
   const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
-    resolver: yupResolver(FormSchema)
+    resolver: yupResolver(FormSchema),
+    mode: "onTouched"
   })
   const onSubmit: SubmitHandler<FormData> = data => console.log(data)
 
@@ -33,9 +55,11 @@ export function Form(props: FormProps) {
     <FormContainer>
       <GoBack onClick={handleModal} />
       <Title>New Invoice</Title>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+
         <Fieldset>
           <Legend>Bill From</Legend>
+
           <InputBlock className={errors.senderStreetAddress && 'error'}>
             <Label htmlFor="sender-street-address">Street Address</Label>
             <TextInput
@@ -43,9 +67,10 @@ export function Form(props: FormProps) {
               id="sender-street-address"
               {...register("senderStreetAddress")}
             />
-            {errors.senderStreetAddress && <InputError>not valid</InputError>}
           </InputBlock>
+
           <SmallInputsArea>
+
             <InputBlock className={errors.senderCity && 'error'}>
               <Label htmlFor="sender-city">City</Label>
               <TextInput
@@ -53,8 +78,8 @@ export function Form(props: FormProps) {
                 id="sender-city"
                 {...register("senderCity")}
               />
-              {errors.senderCity && <InputError>not valid</InputError>}
             </InputBlock>
+
             <InputBlock className={errors.senderPostCode && 'error'}>
               <Label htmlFor="sender-post-code">Post Code</Label>
               <TextInput
@@ -62,108 +87,84 @@ export function Form(props: FormProps) {
                 id="sender-post-code"
                 {...register("senderPostCode")}
               />
-              {errors.senderPostCode && <InputError>not valid</InputError>}
             </InputBlock>
+
             <InputBlock className={`last-child ${errors.senderCountry && 'error'}`}>
-              <Label htmlFor="sender-coutry">Country</Label>
+              <Label htmlFor="sender-country">Country</Label>
               <TextInput
                 type="text"
                 id="sender-country"
                 {...register("senderCountry")}
               />
-              {errors.senderCity && <InputError>not valid</InputError>}
             </InputBlock>
+
           </SmallInputsArea>
         </Fieldset>
+
+        <Fieldset>
+          <Legend>Bill To</Legend>
+
+          <InputBlock className={errors.clientName && 'error'}>
+            <Label htmlFor="client-name">Client's Name</Label>
+            <TextInput
+              type="text"
+              id="client-name"
+              {...register("clientName")}
+            />
+          </InputBlock>
+
+          <InputBlock className={errors.clientEmail && 'error'}>
+            <Label htmlFor="client-email">Client's Email</Label>
+            <TextInput
+              type="text"
+              id="client-email"
+              {...register("clientEmail")}
+            />
+          </InputBlock>
+
+          <InputBlock className={errors.clientStreetAddress && 'error'}>
+            <Label htmlFor="client-street-address">Street Address</Label>
+            <TextInput
+              type="text"
+              id="client-street-address"
+              {...register("clientStreetAddress")}
+            />
+          </InputBlock>
+
+          <SmallInputsArea>
+
+            <InputBlock className={errors.clientCity && 'error'}>
+              <Label htmlFor="client-city">City</Label>
+              <TextInput
+                type="text"
+                id="client-city"
+                {...register("clientCity")}
+              />
+            </InputBlock>
+
+            <InputBlock className={errors.clientPostCode && 'error'}>
+              <Label htmlFor="client-post-code">Post Code</Label>
+              <TextInput
+                type="text"
+                id="client-post-code"
+                {...register("clientPostCode")}
+              />
+            </InputBlock>
+
+            <InputBlock className={`last-child ${errors.clientCountry && 'error'}`}>
+              <Label htmlFor="client-coutry">Country</Label>
+              <TextInput
+                type="text"
+                id="client-country"
+                {...register("clientCountry")}
+              />
+            </InputBlock>
+
+          </SmallInputsArea>
+        </Fieldset>
+
         <button type="submit">teeeest</button>
       </form>
     </FormContainer>
   )
 }
-
-const FormContainer = styled.div`
-  max-width: 72rem;
-  width: 100%;
-  background: var(--color-nearly-black);
-  position: absolute;
-  z-index: 900;
-  top: 0;
-  bottom: 0;
-  padding: 10.4rem 2.4rem 0;
-
-  .error {
-    label {
-      color: var(--color-red);
-    }
-
-    input {
-      border: 1px solid var(--color-red);
-    }
-  }
-
-  button {
-    margin-top: 10rem;
-  }
-`
-
-const Title = styled.h1`
-  margin: 2.4rem 0;
-  font-weight: bold;
-  font-size: 2.4rem;
-  line-height: 3.2rem;
-  color: var(--color-white);
-`
-
-const Fieldset = styled.fieldset`
-  border: none; 
-`
-
-const Legend = styled.legend`
-  color: var(--color-purple);
-  font-size: 1.3rem;
-  line-height: 1.5rem;
-  font-weight: bold;
-`
-
-const InputBlock = styled.div`
-  margin-top: 2.4rem;
-  position: relative;
-`
-
-const Label = styled.label`
-  display: block;
-  line-height: 1.5rem;
-  font-size: 1.2rem;
-  font-weight: 500;
-  color: var(--color-semi-gray);
-  margin-bottom: 1rem;
-`
-
-const TextInput = styled.input`
-  width: 100%;
-  border-radius: 0.4rem;
-  height: 4.8rem;
-  border: 1px solid var(--color-very-light-black);
-  background: var(--color-light-black);
-  font-size: 1.2rem;
-  line-height: 1.5rem;
-  font-weight: bold;
-  color: var(--color-white);
-  padding: 0 2rem;
-`
-
-const SmallInputsArea = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 0 2.3rem; 
-
-  .last-child {
-    grid-column: 1 /span 2;
-  }
-`
-
-const InputError = styled(Label)`
-  position: absolute;
-  top: 0;
-  right: 0;
-`
