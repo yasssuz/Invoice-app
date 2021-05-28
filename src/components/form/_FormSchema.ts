e7f39aeaa392bc64a83f515402ae92e78eb5be3c
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-28 16:33:12 +0200

    format updated

--- src/components/form/_FormSchema.ts
+++ src/components/form/_FormSchema.ts
@@ -1,19 +1,20 @@
 import * as yup from 'yup'
 
 export const FormSchema = yup.object().shape({
-  senderStreetAddress: yup.string().required(),
-  senderCity: yup.string().matches(/^[aA-zZ\s]+$/).required(),
-  senderPostCode: yup.string().required(),
-  senderCountry: yup.string().matches(/^[aA-zZ\s]+$/).required(),
+  senderAddress: yup.object().shape({
+    street: yup.string().required(),
+    city: yup.string().matches(/^[aA-zZ\s]+$/).required(),
+    postCode: yup.string().required(),
+    country: yup.string().matches(/^[aA-zZ\s]+$/).required(),
+  }),
   clientName: yup.string().matches(/^[aA-zZ\s]+$/).required(),
   clientEmail: yup.string().email().required(),
-  clientStreetAddress: yup.string().required(),
-  clientCity: yup.string().matches(/^[aA-zZ\s]+$/).required(),
-  clientPostCode: yup.string().required(),
-  clientCountry: yup.string().matches(/^[aA-zZ\s]+$/).required(),
+  clientAddress: yup.object().shape({
+    street: yup.string().required(),
+    city: yup.string().matches(/^[aA-zZ\s]+$/).required(),
+    postCode: yup.string().required(),
+    country: yup.string().matches(/^[aA-zZ\s]+$/).required(),
+  }),
   invoiceDate: yup.date().required(),
   description: yup.string().required(),
-  itemName: yup.string().required(),
-  itemQuantity: yup.number().required(),
-  itemPrice: yup.number().required()
 })
