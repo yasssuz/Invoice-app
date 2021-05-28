d93a898a2cc084f93b2575a5b62a662286d83517
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-28 16:34:43 +0200

    ready for testing on mobile

--- src/components/form/_Form.tsx
+++ src/components/form/_Form.tsx
@@ -1,4 +1,8 @@
-import { SubmitHandler, useForm } from 'react-hook-form'
+import {
+  SubmitHandler,
+  useForm,
+  useFieldArray,
+} from 'react-hook-form'
 import { yupResolver } from "@hookform/resolvers/yup";
 import { GoBack } from '../shared/_GoBack'
 import {
@@ -14,72 +18,97 @@ import {
   Option,
   LegendItem,
   ItemInfoArea,
-  Total
+  Total,
+  AddItemBtn
 } from './Form.styles'
 import { FormSchema } from './_FormSchema'
 import { BottomBar } from './_BottomBar';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { formatMoneyAmount } from '../../utils/formatters';
+import { StorageContext } from '../../contexts/StorageContext';
+import { listenerCount } from 'node:events';
 
-interface FormProps {
+type FormProps = {
   handleModal: () => void
 }
 
-interface FormData {
-  senderStreetAddress: string
-  senderCity: string
-  senderPostCode: string
-  senderCountry: string
+type FormData = {
   clientName: string
   clientEmail: string
-  clientStreetAddress: string
-  clientCity: string
-  clientPostCode: string
-  clientCountry: string
   invoiceDate: Date
   paymentTerms: any
   description: string
-  itemName: string
-  itemQuantity: number
-  itemPrice: number
-  itemTotal: number
+  senderAddress: {
+    street: string
+    city: string
+    postCode: string
+    country: string
+  }
+  clientAddress: {
+    street: string
+    city: string
+    postCode: string
+    country: string
+  }
+  items: {
+    name: string
+    quantity: number
+    price: number
+  }[]
 }
 
 export function Form(props: FormProps) {
+  const { addInvoice } = useContext(StorageContext)
   const { handleModal } = props
-  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+  const { register, control, handleSubmit, formState: { errors } } = useForm<FormData>({
     resolver: yupResolver(FormSchema),
-    mode: "onTouched"
+    mode: "onTouched",
+    defaultValues: {
+      items: [{ name: undefined, quantity: 1, price: undefined }]
+    },
   })
   const onSubmit: SubmitHandler<FormData> = data => {
-    console.log(data)
-    // const newInvoice = {
-    //   id: '209320',
-    //   createdAt: data.invoiceDate,
-    //   paymentDue: data.paymentTerms,
-    //   description: data.description,
-    //   paymentTerms: data.paymentTerms,
-    //   clientName: data.clientName,
-    //   clientEmail: data.clientEmail,
-    //   status: 'pending',
-    //   senderAddress: {
-    //     street: data.senderStreetAddress,
-    //     city: data.senderCity,
-    //     postCode: data.senderPostCode,
-    //     country: data.senderCountry
-    //   },
-    //   clientAddress: {
-    //     street: data.clientStreetAddress,
-    //     city: data.clientCity,
-    //     postCode: data.clientPostCode,
-    //     country: data.clientCountry
-    //   },
-    // }
-    // console.log(newInvoice)
+    let quantity = 0
+    let price = 0
+
+    data.items.map(item => {
+      quantity = Number(item.quantity) + quantity
+      price = Number(item.price) + price
+    })
+
+    let total = quantity * price
+    const invoiceData = {
+      ...data,
+      id: getRandomId(),
+      total: total,
+      status: 'pending'
+    }
+
+    addInvoice(invoiceData)
+    handleModal()
   }
+  const { fields, append, remove } = useFieldArray({
+    name: "items",
+    control
+  })
   const [itemQuantity, setItemQuantity] = useState(1)
   const [itemPrice, setItemPrice] = useState(100.00)
   const total = itemQuantity * itemPrice
+
+  function getRandomId() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789'
+    let result = []
+
+    for (let i = 0; i < 2; i++) {
+      result.push(characters.charAt(Math.floor(Math.random() * characters.length)))
+    }
+    for (let i = 0; i < 4; i++) {
+      result.push(numbers.charAt(Math.floor(Math.random() * numbers.length)))
+    }
+
+    return result.join('')
+  }
 
   return (
     <FormContainer>
@@ -90,41 +119,41 @@ export function Form(props: FormProps) {
         <Fieldset>
           <Legend>Bill From</Legend>
 
-          <InputBlock className={errors.senderStreetAddress && 'error'}>
+          <InputBlock className={errors.senderAddress?.street && 'error'}>
             <Label htmlFor="sender-street-address">Street Address</Label>
             <TextInput
               type="text"
               id="sender-street-address"
-              {...register("senderStreetAddress")}
+              {...register("senderAddress.street")}
             />
           </InputBlock>
 
           <SmallInputsArea>
 
-            <InputBlock className={errors.senderCity && 'error'}>
+            <InputBlock className={errors.senderAddress?.city && 'error'}>
               <Label htmlFor="sender-city">City</Label>
               <TextInput
                 type="text"
                 id="sender-city"
-                {...register("senderCity")}
+                {...register("senderAddress.city")}
               />
             </InputBlock>
 
-            <InputBlock className={errors.senderPostCode && 'error'}>
+            <InputBlock className={errors.senderAddress?.postCode && 'error'}>
               <Label htmlFor="sender-post-code">Post Code</Label>
               <TextInput
                 type="text"
                 id="sender-post-code"
-                {...register("senderPostCode")}
+                {...register("senderAddress.postCode")}
               />
             </InputBlock>
 
-            <InputBlock className={`last-child ${errors.senderCountry && 'error'}`}>
+            <InputBlock className={`last-child ${errors.senderAddress?.country && 'error'}`}>
               <Label htmlFor="sender-country">Country</Label>
               <TextInput
                 type="text"
                 id="sender-country"
-                {...register("senderCountry")}
+                {...register("senderAddress.country")}
               />
             </InputBlock>
 
@@ -152,41 +181,41 @@ export function Form(props: FormProps) {
             />
           </InputBlock>
 
-          <InputBlock className={errors.clientStreetAddress && 'error'}>
+          <InputBlock className={errors.clientAddress?.street && 'error'}>
             <Label htmlFor="client-street-address">Street Address</Label>
             <TextInput
               type="text"
               id="client-street-address"
-              {...register("clientStreetAddress")}
+              {...register("clientAddress.street")}
             />
           </InputBlock>
 
           <SmallInputsArea>
 
-            <InputBlock className={errors.clientCity && 'error'}>
+            <InputBlock className={errors.clientAddress?.city && 'error'}>
               <Label htmlFor="client-city">City</Label>
               <TextInput
                 type="text"
                 id="client-city"
-                {...register("clientCity")}
+                {...register("clientAddress.city")}
               />
             </InputBlock>
 
-            <InputBlock className={errors.clientPostCode && 'error'}>
+            <InputBlock className={errors.clientAddress?.postCode && 'error'}>
               <Label htmlFor="client-post-code">Post Code</Label>
               <TextInput
                 type="text"
                 id="client-post-code"
-                {...register("clientPostCode")}
+                {...register("clientAddress.postCode")}
               />
             </InputBlock>
 
-            <InputBlock className={`last-child ${errors.clientCountry && 'error'}`}>
+            <InputBlock className={`last-child ${errors.clientAddress?.country && 'error'}`}>
               <Label htmlFor="client-coutry">Country</Label>
               <TextInput
                 type="text"
                 id="client-country"
-                {...register("clientCountry")}
+                {...register("clientAddress.country")}
               />
             </InputBlock>
 
@@ -213,10 +242,10 @@ export function Form(props: FormProps) {
               defaultValue="Net 30 Days"
               {...register("paymentTerms")}
             >
-              <Option>Net 1 Day</Option>
-              <Option>Net 7 Days</Option>
-              <Option>Net 14 Days</Option>
-              <Option>Net 30 Days</Option>
+              <Option value={1}>Net 1 Day</Option>
+              <Option value={7}>Net 7 Days</Option>
+              <Option value={14}>Net 14 Days</Option>
+              <Option value={30}>Net 30 Days</Option>
             </Selector>
           </InputBlock>
 
@@ -235,48 +264,68 @@ export function Form(props: FormProps) {
         <Fieldset>
           <LegendItem className="items">Items List</LegendItem>
 
-          <InputBlock className={errors.itemName && 'error'}>
-            <Label htmlFor="item-name">Item Name</Label>
-            <TextInput
-              type="text"
-              id="item-name"
-              {...register("itemName")}
-            />
-          </InputBlock>
+          {fields.map((field, index) => (
+            <div key={field.id}>
+              <InputBlock className={errors.items?.[index]?.name && 'error'}>
+                <Label htmlFor="item-name">Item Name</Label>
+                <TextInput
+                  type="text"
+                  id="item-name"
+                  {...register(`items.${index}.name` as const, {
+                    required: true,
+                    minLength: 20
+                  })}
+                />
+              </InputBlock>
 
-          <ItemInfoArea>
+              <ItemInfoArea>
 
-            <InputBlock className={errors.itemQuantity && 'error'}>
-              <Label htmlFor="item-quantity">Qty.</Label>
-              <TextInput
-                type="number"
-                id="item-quantity"
-                {...register("itemQuantity")}
-                defaultValue="1"
-                onChange={e => setItemQuantity(Number(e.target.value))}
-              // ref={itemQuantity}
-              />
-            </InputBlock>
+                <InputBlock className={errors?.items?.[index]?.quantity ? 'error' : ""}>
+                  <Label htmlFor="item-quantity">Qty.</Label>
+                  <TextInput
+                    type="number"
+                    id="item-quantity"
+                    {...register(`items.${index}.quantity` as const, {
+                      required: true,
+                      min: 10,
+                      minLength: 10
+                    })}
+                    defaultValue="1"
+                    onChange={e => setItemQuantity(Number(e.target.value))}
+                  />
+                </InputBlock>
 
-            <InputBlock className={errors.itemPrice && 'error'}>
-              <Label htmlFor="item-price">Qty.</Label>
-              <TextInput
-                type="number"
-                id="item-price"
-                {...register("itemPrice")}
-                defaultValue="100.00"
-                onChange={e => setItemPrice(Number(e.target.value))}
-              />
-            </InputBlock>
+                <InputBlock className={errors.items?.[index]?.price && 'error'}>
+                  <Label htmlFor="item-price">Price</Label>
+                  <TextInput
+                    type="number"
+                    id="item-price"
+                    {...register(`items.${index}.price` as const, {
+                      required: true,
 
-            <InputBlock>
-              <Label>Total</Label>
-              <Total>{formatMoneyAmount(total)}</Total>
-            </InputBlock>
+                    })}
+                    defaultValue="100.00"
+                    onChange={e => setItemPrice(Number(e.target.value))}
+                  />
+                </InputBlock>
 
-          </ItemInfoArea>
+                <InputBlock>
+                  <Label>Total</Label>
+                  <Total>{formatMoneyAmount(total)}</Total>
+                </InputBlock>
 
+              </ItemInfoArea>
+            </div>
+          ))}
+
+          <AddItemBtn
+            type="button"
+            onClick={() => append({ name: "", quantity: 0, price: 0 })}
+          >
+            + Add New Item
+          </AddItemBtn>
         </Fieldset>
+
 
         <BottomBar handleModal={handleModal} />
       </form>
