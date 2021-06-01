f02713590886eae8b736b3a69657ad593b406772
Karim Yassine <karimwebdevelopment@gmail.com>
2021-06-01 17:11:31 +0200

    overlay 100% functional

---
+++ src/components/shared/_Overlay.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const OverLay = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  top: 0;
+  left: 0;
+  background: hsla(0, 0%, 0%, 0.5);
+`
+
+export function Overlay() {
+  return <OverLay />
+}
