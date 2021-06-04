5dbf204e5ce6316344f8528fb62ff38562a0b25a
Karim Yassine <karimwebdevelopment@gmail.com>
2021-06-04 15:45:38 +0200

    added filterProvider and InvoiceList component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,12 @@
 import { useContext, useEffect, useState, useCallback } from 'react'
 import styled from 'styled-components'
 import { StorageContext } from '../contexts/StorageContext'
-import { Invoice } from '../components/home/_Invoice'
 import { EmptyPage } from '../components/home/_EmptyPage'
 import { Interactions } from '../components/home/_Interactions'
 import { Form } from '../components/form/_Form'
 import { Overlay } from '../components/shared/_Overlay'
-
-interface InvoiceProps {
-  id: string
-  clientName: string
-  createdAt: string
-  total: number
-  status: string
-}
+import { FilterProvider } from '../contexts/FilterContext'
+import { InvoicesList } from '../components/home/_InvoicesList'
 
 export default function Home() {
   const { getInvoices } = useContext(StorageContext)
@@ -31,29 +24,18 @@ export default function Home() {
   }, [])
 
   return (
-    <>
+    <FilterProvider>
       <Main>
         <Interactions handleForm={handleForm} /> {/*Component*/}
         {isEmpty ? (
           <EmptyPage />  /*Component*/
         ) : (
-          <InvoicesList>
-            <ul>
-              {invoices.map((invoice: InvoiceProps) => <Invoice
-                id={invoice.id}
-                client={invoice.clientName}
-                creationDate={invoice.createdAt}
-                amount={invoice.total}
-                status={invoice.status}
-                key={invoice.id}
-              />)} {/*Component*/}
-            </ul>
-          </InvoicesList>
+          <InvoicesList /> /*Component*/
         )}
       </Main>
       {formOpen && <Overlay />} {/*Component*/}
       {formOpen && <Form handleModal={handleForm} />} {/*Component*/}
-    </>
+    </FilterProvider>
   )
 }
 
@@ -72,20 +54,3 @@ const Main = styled.main`
     padding: 0;
   }
 `
-
-const InvoicesList = styled.section`
-  margin-top: 3.2rem;
-
-  @media screen and (min-width: 750px) {
-    margin-top: 5.6rem;
-  }
-
-  @media screen and (min-width: 1000px) {
-    margin-top: 6.5rem;
-  }
-
-  ul {
-    list-style: none;
-  }
-`
-
