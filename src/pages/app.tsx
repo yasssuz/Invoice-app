34b735e42d93d129f4420c92652b4b5c8d6bd709
Karim Yassine <karimwebdevelopment@gmail.com>
2021-06-08 14:42:14 +0200

    storage provider removed

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,19 +1,16 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { StorageProvider } from '../contexts/StorageContext';
 import Home from './Home'
 import InvoiceInfo from './invoice/InvoiceInfo'
 import { Sidebar } from '../components/shared/_Sidebar'
 
 export default function App() {
   return (
-    <StorageProvider>
-      <Router>
-        <Sidebar /> {/*Component*/}
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/invoices/:id" component={InvoiceInfo} />
-        </Switch>
-      </Router>
-    </StorageProvider>
+    <Router>
+      <Sidebar /> {/*Component*/}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/invoices/:id" component={InvoiceInfo} />
+      </Switch>
+    </Router>
   )
 }
