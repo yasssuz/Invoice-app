3285e7d7120d04664e8fba940dc9be505b1be3c7
Karim Yassine <karimwebdevelopment@gmail.com>
2021-06-09 13:57:42 +0200

    removed form component

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -2,18 +2,11 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './Home'
 import InvoiceInfo from './invoice/InvoiceInfo'
 import { Sidebar } from '../components/shared/_Sidebar'
-import { Form } from '../components/form/_Form'
-import { Overlay } from '../components/shared/_Overlay'
-import { useContext } from 'react'
-import { FormContext } from '../contexts/FormContext'
 
 export default function App() {
-  const { formOpen } = useContext(FormContext)
 
   return (
     <Router>
-      {formOpen && <Form />} {/*Component*/}
-      {formOpen && <Overlay />} {/*Component*/}
       <Sidebar /> {/*Component*/}
       <Switch>
         <Route exact path="/">
