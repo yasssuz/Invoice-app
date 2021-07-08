464099ce07e2081425a12669e0419b7b19160180
Karim Yassine <karimwebdevelopment@gmail.com>
2021-07-08 17:59:42 -0300

    prettier refactor

--- src/components/form/_Form.tsx
+++ src/components/form/_Form.tsx
@@ -1,10 +1,6 @@
-import {
-  SubmitHandler,
-  useForm,
-  useFieldArray,
-} from 'react-hook-form'
+import { SubmitHandler, useForm, useFieldArray } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { GoBack } from '../shared/_GoBack'
+import { GoBack } from "../shared/_GoBack";
 import {
   FormContainer,
   Title,
@@ -21,138 +17,150 @@ import {
   Total,
   AddItemBtn,
   DeleteButton,
-  DatesInputArea
-} from './Form.styles'
-import { FormSchema } from './_FormSchema'
-import { BottomBar } from './_BottomBar';
-import { formatIsoDate, formatMoneyAmount } from '../../utils/formatters';
-import { InputContainer } from './_InputContainer'
-import dayjs from 'dayjs'
-import { addInvoice, editInvoice } from '../../utils/storage';
-import { generateRandomId } from '../../utils/generators';
-import { useContext } from 'react';
-import { FormContext } from '../../contexts/FormContext';
-import { useHistory } from 'react-router-dom'
+  DatesInputArea,
+} from "./Form.styles";
+import { FormSchema } from "./_FormSchema";
+import { BottomBar } from "./_BottomBar";
+import { formatIsoDate, formatMoneyAmount } from "../../utils/formatters";
+import { InputContainer } from "./_InputContainer";
+import dayjs from "dayjs";
+import { addInvoice, editInvoice } from "../../utils/storage";
+import { generateRandomId } from "../../utils/generators";
+import { useContext } from "react";
+import { FormContext } from "../../contexts/FormContext";
+import { useHistory } from "react-router-dom";
 
 interface FormData {
-  id?: any
-  createdAt?: string
-  status: string
-  clientName: string
-  clientEmail: string
-  invoiceDate: Date
-  paymentTerms: string
-  description: string
+  id?: any;
+  createdAt?: string;
+  status: string;
+  clientName: string;
+  clientEmail: string;
+  invoiceDate: Date;
+  paymentTerms: string;
+  description: string;
   senderAddress: {
-    street: string
-    city: string
-    postCode: string
-    country: string
-  }
+    street: string;
+    city: string;
+    postCode: string;
+    country: string;
+  };
   clientAddress: {
-    street: string
-    city: string
-    postCode: string
-    country: string
-  }
+    street: string;
+    city: string;
+    postCode: string;
+    country: string;
+  };
   items: {
-    name: string
-    quantity: number
-    price: number
-    total: number
-  }[]
+    name: string;
+    quantity: number;
+    price: number;
+    total: number;
+  }[];
 }
 
 interface FormProps {
-  invoice?: FormData
-  setStatus?: any 
-  status?: string
+  invoice?: FormData;
+  setStatus?: any;
+  status?: string;
 }
 
 export function Form(props: FormProps) {
-  const history = useHistory()
-  const { invoice, setStatus, status } = props
-  const { handleForm, formEdit, handleFormEdit } = useContext(FormContext)
-  const invoiceId = invoice?.id
-  const { register, control, handleSubmit, formState: { errors }, setValue, watch } = useForm<FormData>({
+  const history = useHistory();
+  const { invoice, setStatus, status } = props;
+  const { handleForm, formEdit, handleFormEdit } = useContext(FormContext);
+  const invoiceId = invoice?.id;
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    watch,
+  } = useForm<FormData>({
     resolver: yupResolver(FormSchema),
     defaultValues: {
-      id: formEdit ? invoice?.id : '',
+      id: formEdit ? invoice?.id : "",
       senderAddress: {
-        street: formEdit ? invoice?.senderAddress.street : '',
-        city: formEdit ? invoice?.senderAddress.city : '',
-        postCode: formEdit ? invoice?.senderAddress.postCode : '',
-        country: formEdit ? invoice?.senderAddress.country : '',
+        street: formEdit ? invoice?.senderAddress.street : "",
+        city: formEdit ? invoice?.senderAddress.city : "",
+        postCode: formEdit ? invoice?.senderAddress.postCode : "",
+        country: formEdit ? invoice?.senderAddress.country : "",
       },
-      clientName: formEdit ? invoice?.clientName : '',
-      clientEmail: formEdit ? invoice?.clientEmail : '',
+      clientName: formEdit ? invoice?.clientName : "",
+      clientEmail: formEdit ? invoice?.clientEmail : "",
       clientAddress: {
-        street: formEdit ? invoice?.clientAddress.street : '',
-        city: formEdit ? invoice?.clientAddress.city : '',
-        postCode: formEdit ? invoice?.clientAddress.postCode : '',
-        country: formEdit ? invoice?.clientAddress.country : '',
+        street: formEdit ? invoice?.clientAddress.street : "",
+        city: formEdit ? invoice?.clientAddress.city : "",
+        postCode: formEdit ? invoice?.clientAddress.postCode : "",
+        country: formEdit ? invoice?.clientAddress.country : "",
       },
-      paymentTerms: formEdit ? invoice?.paymentTerms : '30',
-      description: formEdit ? invoice?.description : '',
-      invoiceDate: formEdit ? invoice?.createdAt : '',
-      items: formEdit ? invoice?.items : [{ name: undefined, quantity: 1, price: 100.00, total: 100 }]
+      paymentTerms: formEdit ? invoice?.paymentTerms : "30",
+      description: formEdit ? invoice?.description : "",
+      invoiceDate: formEdit ? invoice?.createdAt : "",
+      items: formEdit
+        ? invoice?.items
+        : [{ name: undefined, quantity: 1, price: 100.0, total: 100.0 }],
     },
     mode: "onTouched",
-  })
+  });
   const onSubmit: SubmitHandler<FormData> = data => {
-
-    let total = 0
+    let total = 0;
 
     data.items.forEach(item => {
-      total = Number(item.total) + total
-    })
+      total = Number(item.total) + total;
+    });
 
     const invoiceData = {
       ...data,
       id: formEdit ? invoice?.id : generateRandomId(),
       createdAt: formatIsoDate(data.invoiceDate),
-      paymentDue: dayjs(data.invoiceDate).add(Number(data.paymentTerms), 'day').format('YYYY-MM-DD'),
+      paymentDue: dayjs(data.invoiceDate)
+        .add(Number(data.paymentTerms), "day")
+        .format("YYYY-MM-DD"),
       total: total,
-      status: formEdit ? invoice?.status : 'pending'
-    }
+      status: formEdit ? invoice?.status : "pending",
+    };
 
-    console.log(invoiceData)
+    console.log(invoiceData);
 
     if (formEdit === true) {
-      handleForm()
-      handleFormEdit()
-      editInvoice(invoiceData)
-      history.push('/')
-      return
+      handleForm();
+      handleFormEdit();
+      editInvoice(invoiceData);
+      history.push("/");
+      return;
     }
 
-    addInvoice(invoiceData)
-    handleForm()
-  }
+    addInvoice(invoiceData);
+    handleForm();
+  };
   const { fields, append, remove } = useFieldArray({
     name: "items",
     control,
-  })
+  });
 
   function handleDraft(): void {
-    let total = 0
-    const draft = watch()
+    let total = 0;
+    const draft = watch();
 
     draft.items.forEach(item => {
-      total = Number(item.total) + total
-    })
+      total = Number(item.total) + total;
+    });
 
     const draftData = {
       ...draft,
       id: generateRandomId(),
       createdAt: formatIsoDate(draft.invoiceDate),
-      paymentDue: dayjs(draft.invoiceDate).add(Number(draft.paymentTerms), 'day').format('YYYY-MM-DD'),
+      paymentDue: dayjs(draft.invoiceDate)
+        .add(Number(draft.paymentTerms), "day")
+        .format("YYYY-MM-DD"),
       total: total,
-      status: 'draft'
-    }
+      status: "draft",
+    };
 
-    addInvoice(draftData)
-    handleForm()
+    addInvoice(draftData);
+    handleForm();
   }
 
   return (
@@ -165,41 +173,40 @@ export function Form(props: FormProps) {
           <Title>New Invoice</Title>
         </>
       )}
-      <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-
+      <form onSubmit={handleSubmit(onSubmit)} autoComplete='off'>
         <Fieldset>
           <Legend>Bill From</Legend>
 
           <InputContainer
-            label="Street Address"
-            type="text"
-            registration="senderAddress.street"
-            errors={errors.senderAddress?.street && 'error'}
+            label='Street Address'
+            type='text'
+            registration='senderAddress.street'
+            errors={errors.senderAddress?.street && "error"}
             register={register}
           />
 
           <SmallInputsArea>
             <InputContainer
-              type="text"
-              label="City"
-              registration="senderAddress.city"
-              errors={errors.senderAddress?.city && 'error'}
+              type='text'
+              label='City'
+              registration='senderAddress.city'
+              errors={errors.senderAddress?.city && "error"}
               register={register}
             />
 
             <InputContainer
-              type="text"
-              label="Post Code"
-              registration="senderAddress.postCode"
-              errors={errors.senderAddress?.postCode && 'error'}
+              type='text'
+              label='Post Code'
+              registration='senderAddress.postCode'
+              errors={errors.senderAddress?.postCode && "error"}
               register={register}
             />
 
             <InputContainer
-              type="text"
-              label="Country"
-              registration="senderAddress.country"
-              errors={errors.senderAddress?.country && 'error'}
+              type='text'
+              label='Country'
+              registration='senderAddress.country'
+              errors={errors.senderAddress?.country && "error"}
               register={register}
             />
           </SmallInputsArea>
@@ -209,73 +216,73 @@ export function Form(props: FormProps) {
           <Legend>Bill To</Legend>
 
           <InputContainer
-            type="text"
+            type='text'
             label="Client's Name"
-            registration="clientName"
-            errors={errors.clientName && 'error'}
+            registration='clientName'
+            errors={errors.clientName && "error"}
             register={register}
           />
 
           <InputContainer
-            type="text"
+            type='text'
             label="Client's Email"
-            registration="clientEmail"
-            errors={errors.clientEmail && 'error'}
+            registration='clientEmail'
+            errors={errors.clientEmail && "error"}
             register={register}
           />
 
           <InputContainer
-            type="text"
-            label="Street Address"
-            registration="clientAddress.street"
-            errors={errors.clientAddress?.street && 'error'}
+            type='text'
+            label='Street Address'
+            registration='clientAddress.street'
+            errors={errors.clientAddress?.street && "error"}
             register={register}
           />
 
           <SmallInputsArea>
             <InputContainer
-              type="text"
-              label="City"
-              registration="clientAddress.city"
-              errors={errors.clientAddress?.city && 'error'}
+              type='text'
+              label='City'
+              registration='clientAddress.city'
+              errors={errors.clientAddress?.city && "error"}
               register={register}
             />
 
             <InputContainer
-              type="text"
-              label="Post Code"
-              registration="clientAddress.postCode"
-              errors={errors.clientAddress?.postCode && 'error'}
+              type='text'
+              label='Post Code'
+              registration='clientAddress.postCode'
+              errors={errors.clientAddress?.postCode && "error"}
               register={register}
             />
 
             <InputContainer
-              type="text"
-              label="Country"
-              registration="clientAddress.country"
-              errors={errors.clientAddress?.country && 'error'}
+              type='text'
+              label='Country'
+              registration='clientAddress.country'
+              errors={errors.clientAddress?.country && "error"}
               register={register}
             />
           </SmallInputsArea>
         </Fieldset>
 
         <Fieldset>
-          <Legend style={{ display: 'none' }}>Info</Legend>
+          <Legend style={{ display: "none" }}>Info</Legend>
 
           <DatesInputArea>
             <InputContainer
-              type="date"
-              label="Invoice Date"
-              registration="invoiceDate"
-              errors={errors.invoiceDate && 'error'}
+              type='date'
+              label='Invoice Date'
+              registration='invoiceDate'
+              errors={errors.invoiceDate && "error"}
               register={register}
             />
 
-            <InputBlock className={errors.paymentTerms && 'error'}>
-              <Label htmlFor="payment-terms">Payment Terms</Label>
+            <InputBlock className={errors.paymentTerms && "error"}>
+              <Label htmlFor='payment-terms'>Payment Terms</Label>
               <Selector
-                id="payment-terms"
-                defaultValue="Net 30 Days"
+                id='payment-terms'
+                defaultValue='Net 30 Days'
                 {...register("paymentTerms")}
               >
                 <Option value={1}>Net 1 Day</Option>
@@ -287,61 +294,77 @@ export function Form(props: FormProps) {
           </DatesInputArea>
 
           <InputContainer
-            type="text"
-            label="Project / Description"
-            registration="description"
-            errors={errors.description && 'error'}
+            type='text'
+            label='Project / Description'
+            registration='description'
+            errors={errors.description && "error"}
             register={register}
           />
         </Fieldset>
 
         <Fieldset>
-          <LegendItem className="items">Items List</LegendItem>
+          <LegendItem className='items'>Items List</LegendItem>
 
           {fields.map((field, index) => (
             <ItemInfoArea key={field.id}>
-              <InputBlock className={errors.items?.[index]?.name && 'error'}>
+              <InputBlock className={errors.items?.[index]?.name && "error"}>
                 <Label htmlFor={`item-name[${index}]`}>Item Name</Label>
                 <TextInput
-                  type="text"
+                  type='text'
                   id={`item-name[${index}]`}
                   defaultValue={field.name}
                   {...register(`items.${index}.name` as const)}
                 />
               </InputBlock>
 
-              <InputBlock className={errors?.items?.[index]?.quantity && 'error'}>
-                <Label htmlFor="item-quantity">Qty.</Label>
+              <InputBlock
+                className={errors?.items?.[index]?.quantity && "error"}
+              >
+                <Label htmlFor='item-quantity'>Qty.</Label>
                 <TextInput
-                  type="number"
-                  id="item-quantity"
-                  style={{ appearance: 'none' }}
+                  type='number'
+                  id='item-quantity'
+                  style={{ appearance: "none" }}
                   {...register(`items.${index}.quantity` as const)}
                   name={`items[${index}].quantity`}
                   defaultValue={field.quantity}
                   onChange={e =>
-                    setValue(`items.${index}.total` as any,
-                      formatMoneyAmount(Number((document.querySelector(
-                        `input[name=items\\[${index}\\]\\.price]`
-                      ) as HTMLInputElement).value) * Number(e.target.value))
+                    setValue(
+                      `items.${index}.total` as any,
+                      formatMoneyAmount(
+                        Number(
+                          (
+                            document.querySelector(
+                              `input[name=items\\[${index}\\]\\.price]`
+                            ) as HTMLInputElement
+                          ).value
+                        ) * Number(e.target.value)
+                      )
                     )
                   }
                 />
               </InputBlock>
 
-              <InputBlock className={errors.items?.[index]?.price && 'error'}>
-                <Label htmlFor="item-price">Price</Label>
+              <InputBlock className={errors.items?.[index]?.price && "error"}>
+                <Label htmlFor='item-price'>Price</Label>
                 <TextInput
-                  type="number"
-                  id="item-price"
+                  type='number'
+                  id='item-price'
                   {...register(`items.${index}.price` as const)}
                   defaultValue={field.price}
                   name={`items[${index}].price`}
                   onChange={e =>
-                    setValue(`items.${index}.total` as any,
-                      formatMoneyAmount(Number((document.querySelector(
-                        `input[name=items\\[${index}\\]\\.quantity]`
-                      ) as HTMLInputElement).value) * Number(e.target.value))
+                    setValue(
+                      `items.${index}.total` as any,
+                      formatMoneyAmount(
+                        Number(
+                          (
+                            document.querySelector(
+                              `input[name=items\\[${index}\\]\\.quantity]`
+                            ) as HTMLInputElement
+                          ).value
+                        ) * Number(e.target.value)
+                      )
                     )
                   }
                 />
@@ -352,33 +375,39 @@ export function Form(props: FormProps) {
                 <Total
                   readOnly={true}
                   defaultValue={field.total}
-                  id="item-total"
+                  id='item-total'
                   {...register(`items.${index}.total` as const)}
                 />
               </InputBlock>
 
               <DeleteButton onClick={() => remove(index)}>
-                <img src="/assets/icon-delete.svg" alt="delete item" />
+                <img src='/assets/icon-delete.svg' alt='delete item' />
               </DeleteButton>
-
             </ItemInfoArea>
           ))}
 
           <AddItemBtn
-            type="button"
-            onClick={() => append({ name: undefined, quantity: 1, price: 100, total: 100 })}
+            type='button'
+            onClick={() =>
+              append({
+                name: undefined,
+                quantity: 1,
+                price: 100,
+                total: 100,
+              })
+            }
           >
             + Add New Item
           </AddItemBtn>
         </Fieldset>
 
-        <BottomBar 
-          setStatus={setStatus} 
-          id={invoiceId} 
+        <BottomBar
+          setStatus={setStatus}
+          id={invoiceId}
           status={status}
           handleDraft={handleDraft}
         />
       </form>
-    </FormContainer >
-  )
+    </FormContainer>
+  );
 }
